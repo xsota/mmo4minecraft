ad405fb14b16f17222de850ae578f3dfaf01424d
sota <git@xsota.com>
2015-12-17 17:55:54 +0900

    login bonus

---
+++ src/main/java/com/xsota/LoginBonus.java
@@ -0,0 +1,26 @@
+package com.xsota;
+
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * ログインボーナス
+ * @author xsota
+ *
+ */
+
+public class LoginBonus implements Listener{	
+		
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+	
+	}
+	
+	 
+}
